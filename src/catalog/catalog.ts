a6dab16281d5ebf3ecdfe06979f075ccaa3adcb4
ellamartirosyan <ellamartirosyan@gmail.com>
2023-05-09 06:33:38 +0300

    fix: revert

--- src/catalog/catalog.ts
+++
@@ -1,7 +0,0 @@
-import { BBox } from 'geojson';
-
-export interface ICatalog {
-  getCoverageId: (bbox: BBox) => Promise<string>;
-}
-
-export const CATALOG_SYMBOL = Symbol('ICatalog');
--- src/common/errors.ts
+++
@@ -1,7 +0,0 @@
-export class UpstreamUnavailableError extends Error {
-  public constructor(message?: string) {
-    super(message);
-    this.name = 'UpstreamUnavailableError';
-    Object.setPrototypeOf(this, UpstreamUnavailableError.prototype);
-  }
-}
