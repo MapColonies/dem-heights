c6db51688e7ecfec4033c842f60fec8fb611c997
Assaf Chamoy <assafchamoy97@gmail.com>
2023-05-29 00:02:36 +0300

    feat: attempting mock implementations for cesium

--- tests/integration/heights/heights.spec.ts
+++ tests/integration/heights/heights.spec.ts
@@ -1,14 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import jsLogger from '@map-colonies/js-logger';
+import Cesium, { Cartesian2, Cartographic, sampleTerrainMostDetailed, TerrainProvider } from 'cesium';
+import { CesiumTerrainProvider } from 'cesium';
 import { Application } from 'express';
 import httpStatusCodes from 'http-status-codes';
 import { getApp } from '../../../src/app';
+import { SERVICES } from '../../../src/common/constants';
 import { GetHeightsPointsRequest, GetHeightsPointsResponse } from '../../../src/heights/controllers/heightsController';
 import { AdditionalFieldsEnum, PosWithHeight, TerrainTypes } from '../../../src/heights/interfaces';
 import mockJsonPoints from '../../../src/heights/MOCKS/mockdata';
 import { HeightsRequestSender } from './helpers/requestSender';
 
+
 describe('heights', function () {
   const mockJsonData = mockJsonPoints as GetHeightsPointsRequest;
   let requestSender: HeightsRequestSender;
+  let cesiumTerrainProviderFromUrlSpy: jest.SpyInstance;
 
   const basicPositionResponse: PosWithHeight = {
     latitude: 0,
@@ -20,7 +28,44 @@ describe('heights', function () {
   } as PosWithHeight;
 
   beforeAll(async function () {
-    const app = await getApp();
+    cesiumTerrainProviderFromUrlSpy = jest.spyOn(CesiumTerrainProvider, 'fromUrl');
+    
+    //@ts-ignore
+    jest.mock('cesium', () => {
+      const originalCesium = jest.requireActual("cesium");
+
+      return {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        __esModule: true,
+        ...originalCesium,
+        sampleTerrainMostDetailed: jest.fn().mockImplementation(async (provider: CesiumTerrainProvider, positions: Cesium.Cartographic[]): Promise<Cartographic[]> => {
+          return Promise.resolve(positions.map(pos => ({...pos, height: 66}) as Cartographic));
+        })
+      }
+    });
+
+    cesiumTerrainProviderFromUrlSpy.mockReturnValue({
+      availability: {
+        available: true,
+        computeMaximumLevelAtPosition: () => {
+          return 13;
+        }
+      },
+      tilingScheme: {
+        positionToTileXY: () => {
+          const xys = [new Cartesian2(1,2), new Cartesian2(3,4), new Cartesian2(5, 6)];
+          const randomXy = Math.floor(Math.random() * xys.length);
+
+          return xys[randomXy];
+        }
+      }
+    });
+
+    const app = await getApp({
+      override: [
+        { token: SERVICES.LOGGER, provider: { useValue: jsLogger({ enabled: false }) } },
+      ]
+    });
     requestSender = new HeightsRequestSender(app as Application);
   });
 
